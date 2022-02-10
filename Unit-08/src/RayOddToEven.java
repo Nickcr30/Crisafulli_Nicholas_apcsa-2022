34835abfc858a0494959179b7b0ffe0a2f3d74d7
Nickcr30 <nickcr30@hotmail.com>
2022-02-09 20:26:56 -0800

    Unit-08 Complete

--- Unit-08/src/RayOddToEven.java
+++ Unit-08/src/RayOddToEven.java
@@ -5,8 +5,17 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		for (int i = 0; i<ray.length; i++) {
+			if (ray[i] % 2 != 0) {
+				for (int j = i; j<ray.length; j++) {
+					if (ray[j] %2 == 0) {
+						return j-i;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
--- Unit-08/src/SumLastRunner.java
+++ Unit-08/src/SumLastRunner.java
@@ -7,9 +7,33 @@ public class SumLastRunner
 {
 	public static void main( String args[] )
 	{	
-		int[] array = {10,10,10,11,456};
-		System.out.print(RaySumLast.go(array));
 		
+		int[] array1 = {-99,1,2,3,4,5,6,7,8,9,10,5};
+		int[] array2 = {10,9,8,7,6,5,4,3,2,1,-99};
+		int[] array3 = {10,20,30,40,50,-11818,40,30,20,10};
+		int[] array4 = {32767};
+		int[] array5 = {255,255};
+		int[] array6 = {9,10,-88,100,-555,2};
+		int[] array7 = {10,10,10,11,456};
+		int[] array8 = {-111,1,2,3,9,11,20,1};
+		int[] array9 = {9,8,7,6,5,4,3,2,0,-2,6};
+		int[] array10 = {12,15,18,21,23,1000};
+		int[] array11 = {250,19,17,15,13,11,10,9,6,3,2,1,0};
+		int[] array12 = {9,10,-8,10000,-5000,-3000};
+		
+		
+		System.out.println(RaySumLast.go(array1));
+		System.out.println(RaySumLast.go(array2));
+		System.out.println(RaySumLast.go(array3));
+		System.out.println(RaySumLast.go(array4));
+		System.out.println(RaySumLast.go(array5));
+		System.out.println(RaySumLast.go(array6));
+		System.out.println(RaySumLast.go(array7));
+		System.out.println(RaySumLast.go(array8));
+		System.out.println(RaySumLast.go(array9));
+		System.out.println(RaySumLast.go(array10));
+		System.out.println(RaySumLast.go(array11));
+		System.out.println(RaySumLast.go(array12));
 		
 		
 		
