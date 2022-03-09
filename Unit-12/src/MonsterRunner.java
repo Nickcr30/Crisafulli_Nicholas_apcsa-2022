db80d2403b3bfadac74ee6817e41309d55af8397
Nickcr30 <nickcr30@hotmail.com>
2022-03-09 09:50:49 -0800

    Unit-12 Complete

--- Unit-12/src/MonsterRunner.java
+++ Unit-12/src/MonsterRunner.java
@@ -12,11 +12,20 @@ public class MonsterRunner
 		Scanner keyboard = new Scanner(System.in);
 		
 		//ask for name and size
-		
+		System.out.print("type in the name ::");
+		String name = keyboard.next();
+		System.out.print("type in the size ::");
+		int size = keyboard.nextInt();
 		//instantiate monster one
-		
+		Monster one = new Skeleton(name, size);
 		//ask for name and size
-		
+		System.out.print("type in the name ::");
+		name = keyboard.next();
+		System.out.print("type in the size ::");
+		size = keyboard.nextInt();
 		//instantiate monster two
+		Monster two = new Skeleton(name, size);
+		System.out.println("Monster one :: name : " + one.getName() + " size : " + one.getHowBig());
+		System.out.println("Monster two :: name : " + two.getName() + " size : " + two.getHowBig());
 	}
 }
--- Unit-12/src/Skeleton.java
+++ Unit-12/src/Skeleton.java
@@ -7,10 +7,39 @@ import static java.lang.System.*;
 public class Skeleton implements Monster
 {
 	//add instance variables	
-
+	private int size;
+	private String name;
 	//add a constructor
-
+	public Skeleton(String nm, int sz) {
+		size = sz;
+		name = nm;
+	}
 	//add code to implement the Monster interface
-
+	@Override
+	public int getHowBig() {
+		// TODO Auto-generated method stub
+		return size;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	@Override
+	public boolean isBigger(Monster other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isSmaller(Monster other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean namesTheSame(Monster other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 	//add a toString
 }
