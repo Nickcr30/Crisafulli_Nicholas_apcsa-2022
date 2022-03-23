624d7b16aa043cd1a30355bced0efa3306f591c1
Nickcr30 <nickcr30@hotmail.com>
2022-03-23 09:25:57 -0700

    Grid finished

--- Unit-14/src/Grid.java
+++ Unit-14/src/Grid.java
@@ -9,24 +9,74 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[rows][cols];
+		int index = 0;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				grid[i][j] = vals[index];
+				index++;
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		String[] temp = new String[vals.length];
+		for (int p = 0; p < vals.length; p++) {
+			temp[p] = vals[p];
+		}
+		
+		String value = "";
+		int greatest = 0;
+		String most = "";
+		
+		for (int i = 0; i < temp.length; i++) {
+			int count = 1;
+			value = temp[i];
+			if (value.equals("")) {
+				
+			} else {
+				for (int j = i + 1; j < temp.length; j++) {
+					if (temp[j].equals(value)) {
+						count++;
+						temp[j] = "";
+					}
+				}
+				if (count > greatest) {
+					greatest = count;
+					most = temp[i];
+				}
+			}									
+
+		}
+		return most + " occurs the most";
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j].equals(val)) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
 		String output="";
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				output += grid[i][j] + " ";
+			}
+			output += "\n";
+		}
 		return output;
 	}
 }
--- Unit-14/src/GridRunner.java
+++ Unit-14/src/GridRunner.java
@@ -9,7 +9,20 @@ import static java.lang.System.*;
 
 public class GridRunner
 {
+	
 	public static void main( String args[] ) throws IOException
 	{
+		String chars = "abcdefghijklmnopqrstuvwxyz123456789";
+		String[] vals = new String[400];
+		for (int i = 0; i < 400; i++) {
+			int random = (int) (Math.random()*35);
+			String spot = chars.substring(random, random+1);
+			vals[i] = spot;
+		}
+		
+		Grid test = new Grid(20,20, vals);
+		System.out.print(test + "\n");
+		System.out.print(test.findMax(vals));
+		
 	}
 }
