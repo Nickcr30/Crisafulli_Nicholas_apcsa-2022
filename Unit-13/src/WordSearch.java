5cdf5d2758969b60cc3811976b4d95f2fda16caf
Nickcr30 <nickcr30@hotmail.com>
2022-03-30 10:37:25 -0700

    WordSearch

--- Unit-13/src/WordSearch.java
+++ Unit-13/src/WordSearch.java
@@ -11,6 +11,14 @@ public class WordSearch
 
     public WordSearch( int size, String str )
     {
+    	int index = 0;
+    	m = new String[size][size];
+    	for (int r=0; r<size; r++) {
+    		for(int c = 0; c<size;c++) {
+    			m[r][c] = str.substring(index,index+1);
+    			index++;
+    		}
+    	}
     }
 
     public boolean isFound( String word )
@@ -20,41 +28,126 @@ public class WordSearch
 
 	public boolean checkRight(String w, int r, int c)
    {
+		String product = "";
+		if (c <= m.length-w.length()) {
+			for (int i = c; i < r+w.length(); i++) {
+				product += m[r][i];
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public boolean checkLeft(String w, int r, int c)
 	{
+		String product = "";
+		if (r >= w.length()-1) {
+			for (int i = c; i > c-w.length(); i--) {
+				product += m[r][i];
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public boolean checkUp(String w, int r, int c)
 	{
+		String product = "";
+		if (r >= w.length()-1) {
+			for (int i = r; i > r-w.length(); i--) {
+				product += m[i][c];
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public boolean checkDown(String w, int r, int c)
    {
-	   return false;
+		String product = "";
+		if (r <= m.length-w.length()) {
+			for (int i = r; i < r+w.length(); i++) {
+				product += m[i][c];
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	public boolean checkDiagUpRight(String w, int r, int c)
 	{
+		String product = "";
+		if (c <= m.length-w.length() && r >= w.length()-1) {
+			int column = c;
+			for (int i = r; i > r-w.length(); i--) {
+				product += m[i][column];
+				column++;
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	public boolean checkDiagUpLeft(String w, int r, int c)
 	{
+		String product = "";
+		if (c >= w.length()-1 && r >= w.length()-1) {
+			int column = c;
+			for (int i = r; i > r-w.length(); i--) {
+				product += m[i][column];
+				column--;
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	public boolean checkDiagDownLeft(String w, int r, int c)
    {
+		String product = "";
+		if (c <= m.length-w.length() && r <= m.length-w.length()) {
+			int column = c;
+			for (int i = r; i < r+w.length(); i++) {
+				product += m[i][column];
+				column--;
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	public boolean checkDiagDownRight(String w, int r, int c)
 	{
+		String product = "";
+		if (c >= w.length()-1 && r <= m.length-w.length()) {
+			int column = c;
+			for (int i = r; i < r+w.length(); i++) {
+				product += m[i][column];
+				column++;
+			}
+			if (product.equals(w)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
--- Unit-13/src/WordSearchRunner.java
+++ Unit-13/src/WordSearchRunner.java
@@ -12,6 +12,14 @@ public class WordSearchRunner
     	  WordSearch ms = new WordSearch(8,"APPLEXYPXLHJKEEDEGGLLXXCGFPDGOGNMYNTAHUUPUQDGBTSBTHIGHMSILKXLTHIS");
     	  String[] words = "APPLE AXE APEX CAT HEX EGG HAT COMPUTER GUM THIS TUG THIGH".split(" ");
     	  System.out.println( ms );
+    	  System.out.println(ms.checkRight("APPLE",0,0));
+    	  System.out.println(ms.checkDown("AXE",0,0));
+    	  System.out.println(ms.checkLeft("HAT",4,5));
+    	  System.out.println(ms.checkUp("MGE",4,0));
+    	  System.out.println(ms.checkDiagUpRight("ITQTG",7,0));
+    	  System.out.println(ms.checkDiagUpLeft("IMBAD",7,7));
+    	  System.out.println(ms.checkDiagDownLeft("TQTI",4,3));
+    	  System.out.println(ms.checkDiagDownRight("ABMI",4,4));
     	  for( String s : words )
     	  {
     	  	  if( ms.isFound( s) )
