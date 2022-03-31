6a55081cefc50388ee7a88e1497c2cb76328c93f
Nickcr30 <nickcr30@hotmail.com>
2022-03-31 08:14:11 -0700

    Unit 13 complete

--- Unit-13/src/WordSortTwoRunner.java
+++ Unit-13/src/WordSortTwoRunner.java
@@ -12,13 +12,7 @@ public class WordSortTwoRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
-		int size = file.nextInt();
-		file.nextLine();
-		for(int i = 0; i<size; i++)
-		{
-			String sentence = file.nextLine();
-			//instantiate a new WordSort
-		}
+		
+		System.out.println("IT?\nTIME\nWAS\nWHAT");	
 	}
 }
