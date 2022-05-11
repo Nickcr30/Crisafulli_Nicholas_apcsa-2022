066588688ac4bcdecc73726b4e8e39a10134a03d
Nickcr30 <nickcr30@hotmail.com>
2022-05-11 08:13:45 -0700

    Initial push of Unit-17

--- Unit-16/src/PictureTester.java
+++ Unit-16/src/PictureTester.java
@@ -172,7 +172,7 @@ public class PictureTester
     //testCopy();
 	//testMyCollage();
     //testEdgeDetection();
-    testEdgeDetection2();
+    //testEdgeDetection2();
     //testChromakey();
     //testEncodeAndDecode();
     //testGetCountRedOverValue(250);
